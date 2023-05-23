1ea8a9bb824d6dcdeed9bc79f123e160a0d57bf8
shaisham <102400953+shaisham@users.noreply.github.com>
2023-05-23 13:16:42 +0530

    Planting a Tree
    Introduce yourself to users and plant a tree for them!

---
+++ Planting a Tree/src/Tree.java
@@ -0,0 +1,15 @@
+// Define your tree class in this file!
+public class Tree{
+    public static void main(String[] args){
+        //We’re going to introduce ourselves and print a tree to the screen
+        System.out.println("Hey there, I'm Shameer and I’m learning to code in Java!");
+        System.out.println("I’m going to plant a tree today!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+
+    }
+}
